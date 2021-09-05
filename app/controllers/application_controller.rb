8391edbff24f3185bd167d73addb81912014c21f
charcodes24 <charcodes24@gmail.com>
2021-09-05 10:17:08 -0700

    Generated Sessions Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::API
-rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response 
+include ActionContoller::Cookies 
+
+
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response 
+
+
+    
 
 private 
 
