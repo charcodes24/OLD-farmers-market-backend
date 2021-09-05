7820009751426bb94107eee88ff9f6b6a7f39126
charcodes24 <charcodes24@gmail.com>
2021-09-05 10:54:55 -0700

    Deleted practice sessions#index method and created sessions#destroy method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,14 @@
 class SessionsController < ApplicationController
 
-    def index
-        session[:session_hello] ||= "World"
-        cookies[:cookies_hello] ||= "World"
-        byebug
-        render json: { session: session, cookies: cookies.to_hash }
-    end
-
     def create 
         customer = Customer.find_by(username: params[:username])
         session[:customer_id] = customer.id 
         render json: customer 
                                                                                                        
+    end
+
+    def destroy 
+        session.delete :customer_id
+        head :no_content 
     end
 end
