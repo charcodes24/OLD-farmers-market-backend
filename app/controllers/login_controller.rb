58785c09aef6738792315b4b029fe424e9cdfc55
charcodes24 <charcodes24@gmail.com>
2021-09-08 11:15:37 -0700

    Debugged sessions#destroy to logout customer OR vendor

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,7 +2,6 @@ class LoginController < ApplicationController
 
     def show 
         user = Customer.find(session[:customer_id]) || Vendor.find_by(session[:vendor_id])
-        byebug
         if user 
             render json: user 
         else 
